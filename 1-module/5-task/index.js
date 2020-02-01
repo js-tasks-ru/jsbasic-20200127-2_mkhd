bfb6c90af3880183729fcd8ac7ace98498a382ad
Makhmud <insite-it@yandex.ru>
2020-02-01 08:28:28 +0500

    Решена задачка 5-task

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -4,6 +4,15 @@
  * @param {number} maxlength
  * @returns {string}
  */
-function truncate(str, maxlength) {
 
+function truncate(str, maxlength){
+  
+  if (str.length < maxlength){
+    return str;
+  } else{
+    
+    let resultStr = str.slice(0, 19) + '…';
+    return resultStr;
+  }
+  
 }
