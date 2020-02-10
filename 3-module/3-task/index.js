c4771520d3c015fbac07758320982015666e28d0
Makhmud <insite-it@yandex.ru>
2020-02-10 16:14:11 +0500

    3-module/3-task solved

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,4 +3,11 @@
  * @returns {string}
  */
 function camelize(str) {
+  return str
+    .split('-')
+    .map( 
+      (item, index) => 
+      index == 0 ? item : item[0].toUpperCase() + item.slice(1)
+    ).
+    join('');
 }
