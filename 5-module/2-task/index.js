09323622507bf87e6f40090020ffa6e429e36d32
Makhmud <insite-it@yandex.ru>
2020-02-23 01:36:19 +0500

    task 5-2 solved

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -17,13 +17,56 @@ function SortableTable(items) {
   /**
    * @property {Element} - обязательно свойство, которое ссылается на элемент <table>
    */
+  
   this.el = document.createElement('table');
+  this.el.id = "table";
+  let thead = document.createElement('thead');
+  
+  thead.innerHTML = `<tr>
+                     <td>Name</td>
+                     <td>Age</td>
+                     <td>Salary</td>
+                     <td>City</td>
+                     </tr>`;
 
+  this.el.appendChild(thead);  
+  let tBody = document.createElement('tbody');
+  this.el.appendChild(tBody);
+   
+  
+  for (let row of items) {
+    let tr = document.createElement('tr');
+    tr.innerHTML = `<td>${row.name}</td><td>${row.age}</td><td>${row.salary}</td><td>${row.city}</td>`;
+    tBody.appendChild(tr);
+  }
+  
   /**
    * Метод выполняет сортировку таблицы
    * @param {number} column - номер колонки, по которой
    * нужно выполнить сортировку (отсчет начинается от 0)
    * @param {boolean} desc - признак того, что сортировка должна идти в обратном порядке
    */
-  this.sort = (column, desc = false) => {};
+  this.sort = (column, desc = false) => {
+    let sortedRows = Array.from(this.el.tBodies[0].rows)
+    .sort((rowA, rowB) => {
+      if(!desc) {
+        if( isFinite(+rowA.cells[column].innerHTML) ){
+          return +rowA.cells[column].innerHTML > +rowB.cells[column].innerHTML ? 1 : -1;
+        } else {
+          return rowA.cells[column].innerHTML > rowB.cells[column].innerHTML ? 1 : -1;
+        }
+      } else {
+        if( isFinite(+rowA.cells[column].innerHTML) ){
+          return +rowA.cells[column].innerHTML > +rowB.cells[column].innerHTML ? -1 : 1;
+        } else {
+          return rowA.cells[column].innerHTML > rowB.cells[column].innerHTML ? -1 : 1;
+        }
+      }
+      
+    });
+
+    this.el.tBodies[0].append(...sortedRows);
+    
+    
+  };
 }
