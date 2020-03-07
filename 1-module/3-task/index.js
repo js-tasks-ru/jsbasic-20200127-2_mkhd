2f8f01a31a67379fe50919ffbb5dceb57cd9882f
Makhmud <insite-it@yandex.ru>
2020-03-07 13:26:04 +0500

    8-1 in process

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -10,21 +10,3 @@ function ucFirst(str) {
   return str[0].toUpperCase() + str.slice(1);
 }
 
-
-
-
-/*
-  let resultStr = '';
-  
-  if(!str) {
-    return '';
-  } else{
-    let firstChar = str[0].toUpperCase();  
-    resultStr = firstChar;
-  
-    for (let i=1; i < str.length; i++){
-      resultStr += str[i];
-    }
-    return resultStr;
-  }
-*/
--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -4,9 +4,26 @@ class ProductList {
 
   constructor(element) {
     this.el = element;
+    fetch(’/assets/data/products.json’).then().catch();
+    //this.show();
   }
 
   show() {
+    
+    this.renderOuter();
+  }
+
+  renderOuter() {
+    let outerTemplate = `<div class="row justify-content-end">
+    <div class="col-lg-9">
+        <h3 class="section-title">Top Recommendations for You</h3>
+        <div class="row homepage-cards">
+            <!--ВОТ ЗДЕСЬ БУДУТ КАРТОЧКИ ТОВАРОВ-->
+            
+        </div>
+       </div>
+    </div>`;
+    this.el.insertAdjacentHTML('beforeend', outerTemplate);
   }
 }
 
