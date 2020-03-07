d42a4078c10b194e87a30a41a7f5a2d8d7074b17
Makhmud <insite-it@yandex.ru>
2020-03-07 15:48:51 +0500

    8-1 in process

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -5,14 +5,18 @@ class ProductList {
   constructor(element) {
     this.el = element;
     fetch('/assets/data/products.json')
-      .then(response => response.json())
+      .then(response => {
+        response.json();
+        //alert(response[0]);
+      })
       .catch(err => console.log(err));
-    //this.show();
+    
   }
 
   show() {
     
     this.renderOuter();
+    this.renderProduct();
   }
 
   renderOuter() {
@@ -26,6 +30,35 @@ class ProductList {
        </div>
     </div>`;
     this.el.insertAdjacentHTML('beforeend', outerTemplate);
+  }
+
+  renderProduct() {
+    
+    let container = this.el.querySelector("div .homepage-cards");
+    container.innerHTML = `<div data-product-id="1" class="products-list-product col-md-6 col-lg-4 mb-4">
+    <div class="card">
+      <div class="card-img-wrap">
+            <img class="card-img-top" src="https://iliakan.github.io/course-project/assets/images/turntable.png" alt="Card image cap">
+      </div>
+      <div class="card-body">
+            <h5 class="card-title">Victrola Pro USB Bluetooth Turntable Vinyl to MP3 Function</h5>
+            <div class="rate">
+                <i class="icon-star checked"></i>
+                <i class="icon-star checked"></i>
+                <i class="icon-star checked"></i>
+                <i class="icon-star checked"></i>
+                <i class="icon-star checked"></i>
+                <span class="rate-amount ml-2">24</span>
+            </div>
+            <p class="card-text price-text discount"><strong>€ 129.92</strong>
+            <small class="ml-2">€ 250</small></p>
+
+            <button class="product-add-to-cart" data-button-role="add-to-cart">
+              Add to cart
+            </button>
+      </div>
+      </div>
+    </div>`;
   }
 }
 
