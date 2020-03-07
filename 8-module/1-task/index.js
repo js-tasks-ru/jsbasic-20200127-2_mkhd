f04864fe28467c493b7e31ea8a2018c460efd991
Makhmud <insite-it@yandex.ru>
2020-03-07 19:38:45 +0500

    8-1 in process

--- 8-module/1-task/index.js
+++ 8-module/1-task/index.js
@@ -3,9 +3,7 @@ class ProductList {
   productsStoreKey = 'cart-products';
 
   constructor(element) {
-    this.el = element;
-    //this.products = {};
-    
+    this.el = element;    
   }
 
   show() {    
@@ -19,13 +17,11 @@ class ProductList {
     .then((response) => response.json())
     .then((result) => {
       console.log('Результат: ', result);
-      //this.products = result;
       this.products = result;
       this.renderOuter();
-      this.renderProduct(result);
-  
+      this.renderProduct(result);  
+      this.renderReviews(result);
     });
-
   }
 
   renderOuter() {
@@ -45,35 +41,54 @@ class ProductList {
     let container = this.el.querySelector("div .homepage-cards");
     
     for (let product in products) {
-
-      let productCode = `<div data-product-id="1" class="products-list-product col-md-6 col-lg-4 mb-4">
-      <div class="card">
-        <div class="card-img-wrap">
-              <img class="card-img-top" src="${products[product].imageUrl}" alt="Card image cap">
-        </div>
-        <div class="card-body">
+      let productCode = `
+        <div data-product-id="1" class="products-list-product col-md-6 col-lg-4 mb-4">
+        <div class="card">
+          <div class="card-img-wrap">
+            <img class="card-img-top" src="${products[product].imageUrl}" alt="Card image cap"></div>
+            <div class="card-body">
               <h5 class="card-title">${products[product].title}</h5>
               <div class="rate">
-                  <i class="icon-star checked"></i>
-                  <i class="icon-star checked"></i>
-                  <i class="icon-star checked"></i>
-                  <i class="icon-star checked"></i>
-                  <i class="icon-star checked"></i>
-                  <span class="rate-amount ml-2">24</span>
+                  <!-- <i class="icon-star checked"></i> <i class="icon-star checked"></i> <i
+                  class="icon-star checked"></i> <i class="icon-star checked"></i> <i
+                  class="icon-star"></i> -->
+                  
               </div>
-              <p class="card-text price-text discount"><strong>€ 129.92</strong>
-              <small class="ml-2">€ 250</small></p>
-
+              <p class="card-text price-text ${ products[product].oldPrice ? 'discount' : ''}">
+                  <strong>${products[product].price}</strong>
+                  ${ products[product].oldPrice ? '<small class="ml-2">' : ''} 
+                  ${ products[product].oldPrice ? products[product].oldPrice : ''} 
+                  ${ products[product].oldPrice ? '</small>' : ''}
+              </p>
               <button class="product-add-to-cart" data-button-role="add-to-cart">
-                Add to cart
+                  Add to cart
               </button>
+          </div>
         </div>
-        </div>
-      </div>`;
+        </div>`;
 
       container.insertAdjacentHTML('beforeend', productCode);
     }
   }
+
+  renderReviews(products){
+    let container = this.el.querySelectorAll("div .rate");
+    for (let product in products) {
+      if (products[product].rating) {
+        for (let i=0; i<5; i++) {
+          container[product].insertAdjacentHTML('beforeend', `<i class="icon-star
+            ${products[product].rating.stars>i ? ' checked' : ' active' }
+          ">`);
+        }               
+        container[product].insertAdjacentHTML('beforeend', `<span class="rate-amount ml-2">
+          ${products[product].rating.reviewsAmount}
+          </span>`
+        );
+      } 
+    }
+  }
+
+
 }
 
 // Делает класс доступным глобально, сделано для упрощения, чтобы можно было его вызывать из другого скрипта
